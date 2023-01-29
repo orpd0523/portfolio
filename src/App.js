f4f545b830dd9fdc70992d3e6bbf97d0ec13eab6
orpd0523 <orpd0523@gmail.com>
2023-01-29 14:29:05 -0600

    fixed git folder issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import Header from "./components/Header/Header.js" ;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header/>WTF DO I DO??
+      
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header>
+      <ul>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>PROJECTS</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
