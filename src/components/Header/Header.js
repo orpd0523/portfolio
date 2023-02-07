b6fe5a6e76b66b2e91b0dd5c0ee5b1877dc83128
orpd0523 <orpd0523@gmail.com>
2023-02-07 16:15:26 -0600

    added social media icons in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,24 @@
 import "./Header.scss";
-import MenuItems from "../MenuItems/MenuItems";
+// import MenuItems from "../MenuItems/MenuItems";
+import { SiLinkedin } from "react-icons/si";
+import { SiGithub } from "react-icons/si";
+import { MdEmail } from "react-icons/md";
 
 function Header() {
   return (
     <div className="header">
-      <MenuItems/>
+      {/* <MenuItems/> */}
+      <div className="header__main">
+        <div className="header__container">
+          <SiLinkedin className="header__icon" size={22} />
+        </div>
+        <div className="header__container">
+          <SiGithub className="header__icon" size={22} />
+        </div>
+        <div className="header__container">
+          <MdEmail className="header__icon" size={22} />
+        </div>
+      </div>
     </div>
   );
 }
