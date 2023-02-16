05148dafcfac493fd4a07d0ea9ad139518771831
orpd0523 <orpd0523@gmail.com>
2023-02-15 20:08:33 -0600

    added logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,17 @@ import "./Header.scss";
 import { SiLinkedin } from "react-icons/si";
 import { SiGithub } from "react-icons/si";
 import { MdEmail } from "react-icons/md";
+import logo from "../../assets/logo.svg";
 
 function Header() {
   return (
     <div className="header">
       {/* <MenuItems/> */}
+
       <div className="header__main">
+        <div className="header__center">
+          <img src={logo} className="header__logo" />
+        </div>
         <div className="header__container">
           <a href="http://www.linkedin.com/in/operez0523-dev">
             <SiLinkedin className="header__icon" size={22} />
