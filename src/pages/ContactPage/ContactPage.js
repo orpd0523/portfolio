93b22571c2b27bb6ab48d1dfcd9621c67d9c0a09
orpd0523 <orpd0523@gmail.com>
2023-02-19 02:03:53 -0600

    created responsive design for desktop and created contact form

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -2,16 +2,22 @@ import "./ContactPage.scss";
 import Card from "../../components/Card/Card";
 import Button from "../../components/Button/Button";
 import Textfield from "../../components/Textfield/Textfield";
+import { SiLinkedin } from "react-icons/si";
+import { AiFillPhone } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
+import { BsPersonFill } from "react-icons/bs";
 
 function ContactPage() {
   return (
     <div className="contact">
       <div className="contact__container">
-          <h1 className="contact__outside">Let's Work Together!</h1>
+        <h1 className="contact__outside">Let's Work Together!</h1>
         <Card color="contact1">
           <h1 className="contact__title1">Let's Work Together!</h1>
           <div className="contact__box">
-            <h2 className="contact__title2">I look forward to hear from you...</h2>
+            <h2 className="contact__title2">
+              I look forward to hear from you...
+            </h2>
           </div>
           <Textfield>textfield will go here</Textfield>
           <Button variant="contact" type="submit">
@@ -22,19 +28,54 @@ function ContactPage() {
 
       <div className="contact__container">
         <Card color="contact2">
-          <h2>Contact Info Card</h2>
-          <h3>Contact Information:</h3>
-          <div>
-            <h4>Olga Pérez</h4>
+          <h2 className="contact__title3">Contact Information:</h2>
+
+          <div className="contact__square">
+            <div className="hero__hold">
+                <BsPersonFill className="hero__icon" size={22} />
+            </div>
+            <h3 className="contact__social">
+              Olga Pérez
+            </h3>
           </div>
-          <div>
-            <h4>email here</h4>
+
+          <div className="contact__square">
+            <div className="hero__hold">
+              <a href="tel:+1832-790-0105">
+                <AiFillPhone className="hero__icon" size={22} />
+              </a>
+            </div>
+            <a href="tel:+1832-790-0105" className="contact__social">
+              832-790-0105
+            </a>
           </div>
-          <div>
-            <h4>phone number here</h4>
+
+          <div className="contact__square">
+            <div className="hero__hold">
+              <a href="mailto:orpd0523@gmail.com?subject=">
+                <MdEmail className="hero__icon" size={22} />
+              </a>
+            </div>
+            <a
+              href="mailto:orpd0523@gmail.com?subject="
+              className="contact__social"
+            >
+              orpd0523@gmail.com
+            </a>
           </div>
-          <div>
-            <h4>linked in here</h4>
+
+          <div className="contact__square">
+            <div className="hero__hold">
+              <a href="http://www.linkedin.com/in/operez0523-dev">
+                <SiLinkedin className="hero__icon" size={22} />
+              </a>
+            </div>
+            <a
+              href="http://www.linkedin.com/in/operez0523-dev"
+              className="contact__social"
+            >
+              operez0523-dev
+            </a>
           </div>
         </Card>
       </div>
